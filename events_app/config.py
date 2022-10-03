ce0d629476940ae0e438e00c7f11f936d8435e43
Ahyeon Jeon <ahyeonjeon@Ahyeons-MBP.attlocal.net>
2022-10-03 01:09:33 -0700

    fix:config.py

--- events_app/config.py
+++ events_app/config.py
@@ -6,6 +6,6 @@ load_dotenv()
 
 class Config(object):
     """Set environment variables."""
-
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI='sqlite:///database.db'
+    # SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
